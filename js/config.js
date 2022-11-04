106068536b5c59cef504302854dab63dc8b74f66
juan <juanmiber@gmail.com>
2022-11-04 12:03:14 -0500

    modificaciñon del archivo confgig

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_lrC5AALoy', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6blrn9t15rrelfq1k95sq0brin', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
